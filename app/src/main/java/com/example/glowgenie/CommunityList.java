66791927d3602719e2265aee0a02ae2b500c9ec7
RahimiillongT <rahimiillong@student.ub.ac.id>
2024-04-07 21:35:12 +0700

    COMMUNITY 1

--- app/src/main/java/com/example/glowgenie/CommunityList.java
+++ app/src/main/java/com/example/glowgenie/CommunityList.java
@@ -1,6 +1,5 @@
 package com.example.glowgenie;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,29 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Community1 extends AppCompatActivity implements View.OnClickListener{
+public class CommunityList extends AppCompatActivity implements View.OnClickListener {
 
-    TextView seeMore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_community1);
+        setContentView(R.layout.activity_community_list);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        seeMore = findViewById(R.id.seeMore);
-        seeMore.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == seeMore.getId()){
-            Intent intent = new Intent(Community1.this, CommunityList.class);
-            startActivity(intent);
-        }
+
     }
 }
