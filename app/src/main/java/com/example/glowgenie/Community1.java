b897452cc887faebcc45b8e9a14aeb73548253e9
RahimiillongT <rahimiillong@student.ub.ac.id>
2024-04-08 03:52:43 +0700

    COMMUNITY 1

--- app/src/main/java/com/example/glowgenie/Community1.java
+++ app/src/main/java/com/example/glowgenie/Community1.java
@@ -3,6 +3,8 @@ package com.example.glowgenie;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class Community1 extends AppCompatActivity implements View.OnClickListener{
 
     TextView seeMore;
+    Button join1, join2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +30,24 @@ public class Community1 extends AppCompatActivity implements View.OnClickListene
         });
 
         seeMore = findViewById(R.id.seeMore);
+        join1 = findViewById(R.id.join);
+        join2 = findViewById(R.id.join2);
+
         seeMore.setOnClickListener(this);
+        join1.setOnClickListener(this);
+        join2.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == seeMore.getId()){
             Intent intent = new Intent(Community1.this, CommunityList.class);
+            startActivity(intent);
+        }
+
+        if (v.getId() == join1.getId() || v.getId() == join2.getId()){
+            Intent intent = new Intent(Community1.this, CommunityDetail.class);
             startActivity(intent);
         }
     }
