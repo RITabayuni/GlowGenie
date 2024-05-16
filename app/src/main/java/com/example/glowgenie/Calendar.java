517f3fa57e18e6a700cd9633ae532f20935b6d51
Anita Silalahi <anitasilalahi18@student.ub.ac.id>
2024-05-16 08:34:05 +0700

    calendar

---
+++ app/src/main/java/com/example/glowgenie/Calendar.java
@@ -0,0 +1,36 @@
+package com.example.glowgenie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Calendar extends AppCompatActivity {
+    private LinearLayout btn_layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_calendar);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn_layout = findViewById(R.id.btn_lay);
+        btn_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Calendar.this,Calendar2.class));
+            }
+        });
+    }
+}
