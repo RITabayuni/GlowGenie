26a29a6f4ef0ee04248bfc47c742fbd61e0f8d52
Anita Silalahi <anitasilalahi18@student.ub.ac.id>
2024-05-16 14:22:13 +0700

    calendar

---
+++ app/src/main/java/com/example/glowgenie/Register.java
@@ -0,0 +1,32 @@
+package com.example.glowgenie;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import  androidx.appcompat.app.AppCompatActivity ;
+        import  android.os.Bundle ;
+        import  android.view.View ;
+        import  android.widget.AdapterView ;
+        import  android.widget.ArrayAdapter ;
+        import  android.widget.Spinner ;
+        import  android.widget.Toast ;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
