f7d1365916b162b81dfa99da014542a4fdcbcf92
Anita Silalahi <anitasilalahi18@student.ub.ac.id>
2024-05-16 14:45:29 +0700

    calendar

--- app/src/main/java/com/example/glowgenie/MainActivity.java
+++ app/src/main/java/com/example/glowgenie/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.glowgenie.calendar.Calendar;
 import com.example.glowgenie.community.Community1;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     RelativeLayout community;
+    TextView seem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         community = findViewById(R.id.community);
         community.setOnClickListener(this);
+
+        seem = findViewById(R.id.seeMore);
+        seem.setOnClickListener(this);
     }
 
     @Override
@@ -32,5 +38,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(MainActivity.this, Community1.class);
             startActivity(intent);
         }
+
+        if (v.getId() == seem.getId()) {
+            Intent intent = new Intent(MainActivity.this, Calendar.class);
+            startActivity(intent);
+        }
     }
+
 }
