3eb02aa85f1509243d5c420fbb8d4edb3489cc7f
Cindyzky <cindyzakya@student.ub.ac.id>
2024-05-16 08:41:39 +0700

    koreksi manifest part2

--- app/src/main/java/com/example/glowgenie/calendar/Calendar2.java
+++ app/src/main/java/com/example/glowgenie/calendar/Calendar2.java
@@ -1,9 +1,6 @@
-package com.example.glowgenie;
+package com.example.glowgenie.calendar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Calendar extends AppCompatActivity {
-    private LinearLayout btn_layout;
+import com.example.glowgenie.R;
+
+public class Calendar2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_calendar2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        btn_layout = findViewById(R.id.btn_lay);
-        btn_layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Calendar.this,Calendar2.class));
-            }
         });
     }
 }
