3c2b7a11a79ebe6598258f1b344e11808a56b5d0
Anita Silalahi <anitasilalahi18@student.ub.ac.id>
2024-05-26 10:05:15 +0700

    calendar

--- app/src/main/java/com/example/glowgenie/calendar/Calendar.java
+++ app/src/main/java/com/example/glowgenie/calendar/Calendar.java
@@ -3,6 +3,8 @@ package com.example.glowgenie.calendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
@@ -13,42 +15,52 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.glowgenie.MainActivity;
 import com.example.glowgenie.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Calendar extends AppCompatActivity {
-    private LinearLayout btn_layout;
+public class Calendar extends AppCompatActivity implements View.OnClickListener{
+    LinearLayout Day5;
+    ImageView Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calendar);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        btn_layout = findViewById(R.id.btn_lay);
-        btn_layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Calendar.this,Calendar2.class));
-            }
-        });
+        Day5 = findViewById(R.id.day5);
+        Back = findViewById(R.id.back);
+
+        Day5.setOnClickListener(this);
+        Back.setOnClickListener(this);
+
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
 
         List<Item> items = new ArrayList<Item>();
         items.add(new Item("Today", "Hydrating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
-        items.add(new Item("Tommorow", "Exfoliating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
-        items.add(new Item("Today", "Skin Barrier Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
-        items.add(new Item("Today", "Hydrating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Tomorrow", "Exfoliating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Wednesday", "Skin Barrier Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Thursday", "Hydrating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Friday", "Hydrating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Tomorrow", "Exfoliating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Wednesday", "Skin Barrier Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new CalenderAdapter(getApplicationContext(),items));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == Day5.getId()) {
+            Intent intent = new Intent(Calendar.this, Calendar2.class);
+            startActivity(intent);
+        } else if (v.getId() == Back.getId()) {
+            Intent intent = new Intent(Calendar.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/glowgenie/calendar/Calendar2.java
+++ app/src/main/java/com/example/glowgenie/calendar/Calendar2.java
@@ -1,26 +1,67 @@
 package com.example.glowgenie.calendar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.glowgenie.MainActivity;
 import com.example.glowgenie.R;
 
-public class Calendar2 extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calendar2 extends AppCompatActivity implements View.OnClickListener{
+    LinearLayout Day3;
+    ImageView Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calendar2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item("Today", "Hydrating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Tomorrow", "Exfoliating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Wednesday", "Skin Barrier Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Thursday", "Hydrating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Friday", "Hydrating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Tomorrow", "Exfoliating Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+        items.add(new Item("Wednesday", "Skin Barrier Day", "Face Wash \n Toner \n Serum \n Moisturizer \n Sunscreen", "Cleansing Oil \n FaceWash \n Toner \n Moisturizer"));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new CalenderAdapter(getApplicationContext(),items));
+
+        Day3 = findViewById(R.id.day3);
+        Back = findViewById(R.id.back);
+
+        Day3.setOnClickListener(this);
+
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == Day3.getId()){
+            Intent intent = new Intent(Calendar2.this, Calendar.class);
+            startActivity(intent);
+        } else if (v.getId() == Back.getId()) {
+            Intent intent = new Intent(Calendar2.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
